16cca547e985da9d482e7e69ab2420d7c866e08b
Daniele206 <danieledeieso2004@gmail.com>
2024-05-08 15:50:50 +0200

    [add] hom.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.css' integrity='sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==' crossorigin='anonymous'/>
+    <title>laravel primi passi</title>
+</head>
+<body>
+    <div class="text-center">
+        <h1 class="fw-bold my-4">Laravel primi passi</h1>
+        <h2 class="fw-bold my-4">Home</h2>
+    </div>
+</body>
+</html>
